966d7ee11e721bb9fadadf0397e437fef3e2511c
Estuardo-Sazo <estuardosazo1206@gmail.com>
2021-03-18 14:44:27 -0600

    Creado portafolio Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { WorksComponent } from './works/works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    WorksComponent
   ],
   imports: [
     BrowserModule
